d8e0a28a5a339fe07635fa565d1f2b70dc97459a
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-09-30 17:36:19 -0400

    init

---
+++ data.js
@@ -0,0 +1,167 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.4108236547002182,
+        "pitch": 0.3107922585477283,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4448663076724237,
+          "pitch": 0.032714003155476234,
+          "rotation": 6.283185307179586,
+          "target": "1-area-2"
+        },
+        {
+          "yaw": 1.9682823246916072,
+          "pitch": 0.23426663519703794,
+          "rotation": 6.283185307179586,
+          "target": "2-area-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.07317107789966215,
+        "pitch": 0.3461355229429923,
+        "fov": 1.1223387837181062
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5502761757302466,
+          "pitch": 0.04918783114669445,
+          "rotation": 6.283185307179586,
+          "target": "0-area-1"
+        },
+        {
+          "yaw": -1.063828054002407,
+          "pitch": 0.1218456171160387,
+          "rotation": 6.283185307179586,
+          "target": "2-area-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-area-3",
+      "name": "Area 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.12374492344352994,
+        "pitch": 0.3029845066667427,
+        "fov": 1.3491839106259393
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8050478317590466,
+          "pitch": -0.12168374365908896,
+          "rotation": 12.566370614359176,
+          "target": "1-area-2"
+        },
+        {
+          "yaw": -1.0786648519698296,
+          "pitch": -0.11619296680753521,
+          "rotation": 6.283185307179586,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Shop Branch 9-30-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
